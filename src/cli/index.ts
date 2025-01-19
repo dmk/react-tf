8e41a0216c01c18029b96bacb84b691dc07a8672
Dmytro Koval <contact@dkoval.pro>
2025-01-20 00:51:47 +0200

    Release v0.3.0

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,7 +5,7 @@ import { processFiles } from './processor';
 program
   .name('react-tf')
   .description('React Terraform renderer CLI')
-  .version('0.2.2')
+  .version('0.3.0')
   .argument('[paths...]', 'Directory or specific JSX/TSX files to process', ['.'])
   .option('-o, --output <directory>', 'Output directory for generated Terraform files')
   .action(processFiles);
